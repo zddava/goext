3e0bea01e0e00be6ab410158b83892e0d265fe32
冬冬 <watcherz@qq.com>
2023-10-30 18:49:12 +0800

    20231030

--- system/file.go
+++ system/file.go
@@ -1,6 +1,10 @@
 package system
 
-import "os"
+import (
+	"bufio"
+	"io"
+	"os"
+)
 
 func FileExists(file string) bool {
 	_, err := os.Stat(file)
@@ -8,4 +12,23 @@ func FileExists(file string) bool {
 		return true
 	}
 	return os.IsExist(err)
+}
+
+func ReadLine(r *bufio.Reader) (string, error) {
+	line, isprefix, err := r.ReadLine()
+	for isprefix && err == nil {
+		var next []byte
+		next, isprefix, err = r.ReadLine()
+		line = append(line, next...)
+	}
+
+	return string(line), err
+}
+
+func IsReadError(err error) bool {
+	return err != nil && err != io.EOF
+}
+
+func IsEOF(err error) bool {
+	return err != nil && err == io.EOF
 }
--- system/file_test.go
+++ system/file_test.go
@@ -1,6 +1,9 @@
 package system
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"testing"
 
 	"github.com/zddava/goext/assert"
@@ -9,4 +12,29 @@ import (
 func TestFileExists(t *testing.T) {
 	assert.True(FileExists("file.go"), t)
 	assert.False(FileExists("not_exists"), t)
+}
+
+func TestReadLine(t *testing.T) {
+	f, err := os.Open("file.go")
+	if err != nil {
+		panic(err)
+	}
+
+	defer f.Close()
+
+	r := bufio.NewReader(f)
+	for {
+		line, err := ReadLine(r)
+		if IsReadError(err) {
+			panic(err)
+		}
+
+		if IsEOF(err) {
+			fmt.Println("end of file")
+			break
+		}
+
+		fmt.Println(line)
+	}
+
 }
