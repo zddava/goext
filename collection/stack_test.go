a234980350a186ae622a531ab7782524bc325efb
冬冬 <watcherz@qq.com>
2023-11-20 16:02:53 +0800

    20231120

--- collection/stack_test.go
+++ collection/stack_test.go
@@ -31,4 +31,14 @@ func TestStack(t *testing.T) {
 	assert.Equals("1", e, t)
 	assert.Equals(0, StackLen(stack), t)
 	assert.True(IsStackEmpty(stack), t)
+
+	PushToStack(stack, "1")
+	PushToStack(stack, "2")
+	PushToStack(stack, "3")
+	PushToStack(stack, "4")
+
+	p := PeekFromStack(stack)
+	assert.Equals("4", *p, t)
+	assert.Equals(4, StackLen(stack), t)
+	assert.False(IsStackEmpty(stack), t)
 }
